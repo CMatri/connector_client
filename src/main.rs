0dbf8eeb53c1ea4fddb46f00f103c585a438c370
cmatri <cperkin4@gmu.edu>
2019-10-12 21:41:54 -0400

    Smoother mouse movement

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::str::from_utf8;
 use std::time;
 use enigo::*;
 use winapi::um::mmeapi::{waveOutSetVolume, waveOutGetVolume};
+use lerp::{Lerp, LerpIter};
 
 const BUF_LEN: usize = 20;
 const UDP_PORT: i32 = 5514;
@@ -40,7 +41,7 @@ fn udp_listener() {
     }
 }
 
-fn packet_handler(last_vol: &mut u32, enigo: &mut Enigo, bytes: &[u8; BUF_LEN], tx: mpsc::Sender<(f32, f32)>) {
+fn packet_handler(last_vol: &mut u32, enigo: &mut Enigo, bytes: &[u8; BUF_LEN], tx: &mpsc::Sender<(f64, f64)>) {
     match bytes[0] {
         0x01 => { // Mouse
             let x = &bytes[1..7];
@@ -51,7 +52,8 @@ fn packet_handler(last_vol: &mut u32, enigo: &mut Enigo, bytes: &[u8; BUF_LEN], 
             let mut y_conv = String::from_utf8(y.iter().cloned().collect()).expect("fail").parse::<f32>().unwrap();
             if x_neg { x_conv *= -1.0; }
             if y_neg { y_conv *= -1.0; }
-            tx.send((x_conv * 2.0, y_conv * 4.0));//enigo.mouse_move_relative((x_conv * 2.0) as i32, (y_conv * 4.0) as i32);
+            tx.send(((x_conv * 2.0).into(), (y_conv * 4.0).into())).expect("Failed to send packet to mouse thread.");
+            //enigo.mouse_move_relative((x_conv) as i32, (y_conv) as i32);
         }
         0x02 => { // Click,
             if bytes[1] == 0 {
@@ -76,14 +78,33 @@ fn packet_handler(last_vol: &mut u32, enigo: &mut Enigo, bytes: &[u8; BUF_LEN], 
     }
 }
 
-fn mouse_thread(rx: mpsc::Receiver<(f32, f32)>) {
+fn mouse_thread(rx: mpsc::Receiver<(f64, f64)>) {
     thread::spawn(move || {
-        let mut packet_buf: Vec<(f32, f32)> = Vec::new();
-        let mut cur_pos = (0, 0);
+        let wait_time = time::Duration::from_millis(1);
+        let mut enigo = Enigo::new();
+        let mut cur_pos = (0f64, 0f64);
+        /*let quad_bez = |x0: f64, x1: f64, x2: f64, steps: usize| -> Vec<f64> {
+            (0..steps).map(|i| { 
+                let x_0_1 = x0.lerp(x1, i as f64 / steps as f64);
+                let x_1_2 = x1.lerp(x2, i as f64 / steps as f64);
+                x_0_1.lerp(x_1_2, i as f64 / steps as f64)
+            }).collect()
+        };*/ // To use later
+        let mut lerp = |p0: &(f64, f64), p1: &(f64, f64)| {
+            if p0 == &(0f64, 0f64) { return; } // Prevents glitch on reception of first packet
+            let vals_x: Vec<_> = p0.0.lerp_iter(p1.0, 5).collect();
+            let vals_y: Vec<_> = p0.1.lerp_iter(p1.1, 5).collect();
+            for (i, x) in vals_x.iter().enumerate() {
+                enigo.mouse_move_relative(*x as i32, vals_y[i] as i32);
+                thread::sleep(wait_time);
+            }
+        };
         loop { 
             match rx.recv() {
                 Ok(pak) => { 
-                    
+                    //enigo.mouse_move_relative((pak.0) as i32, (pak.1) as i32)
+                    lerp(&cur_pos, &pak);
+                    cur_pos = pak;
                 }
                 Err(_) => {}
             }
@@ -92,8 +113,8 @@ fn mouse_thread(rx: mpsc::Receiver<(f32, f32)>) {
 }
 
 fn initiate_connection(ip: String, port: i32) {
-    let mut enigo = Enigo::new();
     let mut last_vol = 0;
+    let mut enigo = Enigo::new();
     let (tx, rx) = mpsc::channel();
     mouse_thread(rx);
     match TcpStream::connect(format!("{}:{}", ip, port)) {
@@ -119,7 +140,7 @@ fn initiate_connection(ip: String, port: i32) {
             let mut data = [0 as u8; BUF_LEN];
             loop {
                 match stream.read(&mut data) {
-                    Ok(_) => packet_handler(&mut last_vol, &mut enigo, &data, tx),
+                    Ok(_) => packet_handler(&mut last_vol, &mut enigo, &data, &tx),
                     Err(e) => {
                         println!("Failed to receive data: {}", e);
                         break;
